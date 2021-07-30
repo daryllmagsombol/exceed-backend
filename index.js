fc348e5dc8b94a5ab8a6337bb8ca337585709cb5
Daryll Magsombol <darjosh@192.168.254.107>
2021-07-31 01:52:32 +0800

    revise port

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(`/api/${process.env.APP_VERSION}`, routes)
 
 //Set port from environment variable or set 5000 if not found
 //+ sign converts the port to number since all env values are string
-const port = +process.env.APP_PORT || 6000
+const port = +process.env.PORT || 5000
 //Start the express app on the given port
 //Callback function defines action to be done by the express app upon start
 app.listen(port, () => {
